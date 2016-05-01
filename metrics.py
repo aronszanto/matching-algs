1cbf38ad791f41d43a7cbdec5c31a75a4ae52322
Jerry <kananunroj@hotmail.com>
2016-05-01 16:31:23 -0400

    cardinal envy-free implemented

--- metrics.py
+++ metrics.py
@@ -41,12 +41,29 @@ def list_sd(list1, list2):
 			return False
 	return True	
 
+'''
+Helper function utility:
+Compute the utility an agent has given a probability list
+If agent's cardinal preference/utility is [u0,...,uk] and it is assigned [p0,...,pk]
+then the agent's utility is u0*p0+...+uk*pk
+'''
+def utility(agent, probs):
+	assert len(agent.cardinal_prefs) == len(probs)
+	return sum([agent.cardinal_prefs[i]*probs[i] for i in xrange(len(probs))])
 
 def envyfree_sd(agent_pairs):
 	for (curr_agent, curr_probs) in agent_pairs:
 		for (other_agent, other_probs) in agent_pairs:
 			# you don't envy yourself, so checking like this is fine
 			if not list_sd( sort_pref(curr_probs, curr_agent.ordinal_prefs), sort_pref(other_probs, curr_agent.ordinal_prefs) ):
+				return False
+	return True
+
+def envyfree_card(agent_pairs):
+	for (curr_agent, curr_probs) in agent_pairs:
+		for (other_agent, other_probs) in agent_pairs:
+			# you don't envy yourself, so checking like this is fine
+			if utility(curr_agent, curr_probs) < utility(curr_agent, other_probs) :
 				return False
 	return True
 
