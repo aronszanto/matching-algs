6658296db9ca34095645da84c154df58f05958a2
aszanto9 <aszanto@college.harvard.edu>
2016-05-02 10:32:54 -0400

    redefined __eq__ in agents

--- agent.py
+++ agent.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
+
 class Agent:
-    def __init__(self, id, item = None, cardinal_prefs = [], ordinal_prefs = []):
+
+    def __init__(self, id, item=None, cardinal_prefs=[], ordinal_prefs=[]):
         self.id = id
         self.item = item
         self.cardinal_prefs = cardinal_prefs
         self.ordinal_prefs = ordinal_prefs
+
+    def __eq__(self, other):
+        return (self.id == other.id and self.cardinal_prefs ==
+                other.cardinal_prefs and self.ordinal_prefs=other.ordinal_prefs)
