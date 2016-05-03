4b3dd7b6bcc80aaeb89f64aa3309fd07c5a5244c
aszanto9 <aszanto@college.harvard.edu>
2016-05-03 10:15:58 -0400

    FINAL ALGOS

--- sim.py
+++ sim.py
@@ -54,11 +54,12 @@ def main():
     # agents = assign_preferences()
 
     # hard-code, for debugging
-    agents = [Agent(id=3, item=0, ordinal_prefs=[0, 2, 3, 1]),
-              Agent(id=1, item=1, ordinal_prefs=[0, 3, 2, 1]),
-              Agent(id=2, item=2, ordinal_prefs=[0, 3, 1, 2]),
-              Agent(id=0, item=3, ordinal_prefs=[0, 3, 1, 2])
-              ]
+    agents = assign_preferences()
+    # [Agent(id=3, item=0, ordinal_prefs=[0, 2, 3, 1]),
+    #           Agent(id=1, item=1, ordinal_prefs=[0, 3, 2, 1]),
+    #           Agent(id=2, item=2, ordinal_prefs=[0, 3, 1, 2]),
+    #           Agent(id=0, item=3, ordinal_prefs=[0, 3, 1, 2])
+    #           ]
 
     print "initial agents"
     logging.debug("INITIAL AGENTS")
