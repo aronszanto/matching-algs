c3190ab16db50dd162422dc2b6c36ba54e31041e
aszanto9 <aszanto@college.harvard.edu>
2016-05-03 10:13:14 -0400

    switching

--- sim.py
+++ sim.py
@@ -54,11 +54,7 @@ def main():
     # agents = assign_preferences()
 
     # hard-code, for debugging
-    agents = [Agent(id=3, item=0, ordinal_prefs=[0, 2, 3, 1]),
-              Agent(id=1, item=1, ordinal_prefs=[0, 3, 2, 1]),
-              Agent(id=2, item=2, ordinal_prefs=[0, 3, 1, 2]),
-              Agent(id=0, item=3, ordinal_prefs=[0, 3, 1, 2])
-              ]
+    agents = assign_preferences()
 
     print "initial agents"
     logging.debug("INITIAL AGENTS")
