3b7c45623801924d878776c3f5882bc9b391138b
aszanto9 <aszanto@college.harvard.edu>
2016-05-02 23:04:54 -0400

    PS done!!

--- prob_serial.py
+++ prob_serial.py
@@ -6,13 +6,17 @@ import config
 import numpy as np
 import logging
 import sim
+from fractions import Fraction
 
 NUM_ROUNDS = 10000
+LIMIT_DENOMINATOR = 100
 item_morsels = [NUM_ROUNDS] * config.NUM_AGENTS
 
 # agent_mat[i][j] is number of morsels of item j that agent i has
 agent_mat = [[0 for _ in xrange(config.NUM_AGENTS)]
              for _ in xrange(config.NUM_AGENTS)]
+agent_mat_fracs = [[0 for _ in xrange(config.NUM_AGENTS)]
+                   for _ in xrange(config.NUM_AGENTS)]
 
 
 def item_available(i):
@@ -21,8 +25,8 @@ def item_available(i):
 
 def get_top_available(a):
     # print str(a) + "'s preference order is " + str(a.ordinal_prefs)
-    for i, o in enumerate(a.ordinal_prefs):
-        if item_available(o):
+    for i in a.ordinal_prefs:
+        if item_available(i):
             # print "item " + str(o) + " available"
             return i
     return -1
@@ -32,10 +36,15 @@ def bite(a, i):
     agent_mat[a.id][i] += 1
     item_morsels[i] -= 1
 
+# PS eating algorithm on agents list, modeled with discrete time. Optional
+# arguments to change default number of rounds, ouput probability
+# distribution as a fractional matrix, and if so, to what extent to limit
+# the denominator in the rational approximation
 
-def prob_serial(agents):
+
+def prob_serial(agents, num_rounds=NUM_ROUNDS, output_fracs=False, limit_denom=LIMIT_DENOMINATOR):
     round = 0
-    while round < NUM_ROUNDS:
+    while round < num_rounds:
         # shuffle agents so that they all get different orders (to maximize
         # stochasticity)
         random.shuffle(agents)
@@ -51,16 +60,16 @@ def prob_serial(agents):
         round += 1
     for i in xrange(config.NUM_AGENTS):
         for j in xrange(config.NUM_AGENTS):
-            agent_mat[i][j] = float(agent_mat[i][j]) / \
-                (NUM_ROUNDS * config.NUM_AGENTS)
+            agent_mat[i][j] = float(agent_mat[i][j]) / NUM_ROUNDS
+            amf = Fraction(
+                agent_mat[i][j]).limit_denominator(limit_denom)
+            agent_mat_fracs[i][j] = str(0) if amf.numerator == 0 else str(
+                amf.numerator) + "/" + str(amf.denominator)
+    return agent_mat_fracs if output_fracs else agent_mat
 
 agents_list = sim.assign_preferences()
-for a in agents_list:
-    a.ordinal_prefs = [0, 1, 2, 3, 4]
-agents_list[0].ordinal_prefs = [4, 3, 2, 1, 0]
-agents_list[1].ordinal_prefs = [4, 3, 2, 1, 0]
+
 # print agents_list
 
-prob_serial(agents_list)
-print agent_mat
-print sum([a[4] for a in agent_mat])
+# print prob_serial(agents_list, output_fracs=True, limit_denom=100)
+print [sum(r) for r in prob_serial(agents_list)]
